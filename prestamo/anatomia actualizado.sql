bbf906558b844315fd7fbabc36a6539e4f49d6ed
kalita09 <kalampz@gmail.com>
2016-05-19 23:36:03 -0600

    comentario

--- prestamo/anatomia actualizado.sql
+++ prestamo/anatomia actualizado.sql
@@ -3,44 +3,44 @@ use Anatomia;
 
 --Tabla Curso
 Create table if not exists `Curso`(
-   Sigla varchar(50),
-   NombreCurso varchar(50),
+   Sigla varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+   NombreCurso varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
    CONSTRAINT Curso_pk PRIMARY KEY (Sigla)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --Tabla Estudiante
 Create table if not exists `Estudiante`(
-   Carnet varchar(50),
-   Rol varchar(50),
-   Contraseña varchar(50);
-   NombreEstudiante varchar(50),
-   Correo varchar(50),
+   Carnet varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+   Rol varchar(50) DEFAULT 'Estudiante' CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+   Contraseña varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+   NombreEstudiante varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+   Correo varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
    CONSTRAINT Estudiante_pk PRIMARY KEY (Carnet)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --Tabla Articulo
 Create table if not exists `Articulo`(
    Id int(11) not null AUTO_INCREMENT,
-   NombreArticulo varchar(50),
-   Lugar varchar(50),
-   Categoria varchar(50),
+   NombreArticulo varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+   Lugar varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+   Categoria varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
    CONSTRAINT Articulo_pk PRIMARY KEY (Id)
 	
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --Tabla Prestamo
 Create table if not exists `Prestamo`(
    Id int(11) not null AUTO_INCREMENT,
    Fecha date NOT NULL,
    Hora time NOT NULL,
-   Entrega Varchar NOT NULL,
-   EstudianteIdFK VARCHAR(50) NOT NULL,
-   CursoIdFK VARCHAR(50) NOT NULL,
+   Entrega Varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+   EstudianteIdFK VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+   CursoIdFK VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
    CONSTRAINT Prestamo_pk PRIMARY KEY (Id)
    ) ENGINE=InnoDB;
 	CONSTRAINT EstudianteId_prestamo_fk FOREIGN KEY (EstudianteIdFK) REFERENCES Estudiante(Carnet) ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT CursoId_prestamo_fk FOREIGN KEY (CursoIdFK) REFERENCES Curso(Sigla) ON UPDATE CASCADE ON DELETE SET NULL
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 ALTER TABLE Prestamo ADD CONSTRAINT EstudianteId_prestamo_fk FOREIGN KEY (EstudianteIdFK) REFERENCES Estudiante(Carnet) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE Prestamo ADD CONSTRAINT CursoId_prestamo_fk FOREIGN KEY (CursoIdFK) REFERENCES Curso(Sigla) ON UPDATE CASCADE ON DELETE CASCADE;
@@ -51,13 +51,13 @@ ALTER TABLE Prestamo ADD CONSTRAINT CursoId_prestamo_fk FOREIGN KEY (CursoIdFK) 
 Create table if not exists `Prestamo_Articulo`(
    PrestamoIdFK int not null,
    ArticuloIdFK int not null,
-   Recibe Varchar NOT NULL,
+   Recibe Varchar (50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
    Cantidad Int not null,
    CONSTRAINT Prestamo_Articulo_pk PRIMARY KEY (PrestamoIdFK, ArticuloIdFK),
    CONSTRAINT prestamoId_prestamo_fk FOREIGN KEY (PrestamoIdFK) REFERENCES Prestamo(Id) ON UPDATE CASCADE ON DELETE CASCADE,
    CONSTRAINT articuloId_prestamo_fk FOREIGN KEY (ArticuloIdFK) REFERENCES Articulo(Id) ON UPDATE CASCADE ON DELETE CASCADE
 	
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --Quitando FK de entrega y recibe ya es innecesario gastar una tabla para solo 3 personas
 ALTER TABLE Prestamo
@@ -69,11 +69,9 @@ DROP FOREIGN KEY EntregaId_prestamo_fk
 ALTER TABLE Prestamo
 DROP FOREIGN KEY RecibeId_prestamo_fk
 
-ALTER TABLE prestamo
-ADD Entrega Varchar(50) NOT NULL AFTER Hora;
 
-ALTER TABLE estudiante
-ADD Correo Varchar(50) NOT NULL AFTER NombreEstudiante;
+
+
 -------------------------------------
 
 CREATE TABLE IF NOT EXISTS `alarms` (
--- prestamo/conexion.php
+++ prestamo/conexion.php
@@ -6,6 +6,7 @@ $dbname = "prueba";
 
 // Create connection
 $conexion = new mysqli($servername, $username, $password, $dbname);
+$mysqli->set_charset("utf8");
 // Check connection
 if ($conexion->connect_error) {
     die("Connection failed: " . $conexion->connect_error);
